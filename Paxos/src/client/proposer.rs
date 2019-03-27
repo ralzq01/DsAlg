097e95efd06562c2c016dede522e996bc3213806
zhiqi lin <ralzq01@gmail.com>
2019-03-27 19:36:55 +0800

    update client

--- Paxos/src/client/proposer.rs
+++ Paxos/src/client/proposer.rs
@@ -1,12 +1,13 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::net::SocketAddr;
 
 use crate::node::Node;
 
 pub trait Propose {
   /// propose a method and ticket to all servers
   fn propose(&self, method: &str, ticket: u32) {
-
+    
   }
 }
 
@@ -14,13 +15,24 @@ pub struct Client {
   port: u32,  // self port
   server_sock_list: Vec<TcpStream>,
   server_port_list: Vec<u32>,
+  tickets: u32,
 }
 
+impl Client {
+  pub fn new(port: u32, server_port_list: &Vec<u32>) -> Client {
+    let mut server_sock_list = vec![];
+    let mut fail_port_idx = vec![];
+    for idx in (0..server_port_list.len()) {
+      let addrs = SocketAddr::from(([127, 0, 0, 1], server_port_list[idx]));
+      if let mut Ok(sock) = TcpStream::connect(&addrs) {
+        server_sock_list.push(sock);
+      } else {
+        println!("client port: {} can't connect with server port: {}", port, server_port);
+        fail_port_idx.push(idx);
+      }
+    }
+  }
+}
 
 impl Node for Client;
 
-
-
-impl Propose for Client {
-  fn propose
-}
--- Paxos/src/main.rs
+++ Paxos/src/main.rs
@@ -1,3 +1,10 @@
+
+mod client;
+use crate::client::Client;
+
+mod server;
+use crate::server::Server;
+
 fn main() {
     println!("Hello, world!");
 }
